53b57260f6d4c514221a396362bf917c4d54628a
tennylaylor <Lennyttaylor@gmail.com>
2024-12-22 15:03:47 -0500

    add selection character

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,11 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-brand">Rick and Morty</div>
-      <div className="nav-links"></div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/forum">Forum</Link>
+      </div>
     </nav>
   );
 }
